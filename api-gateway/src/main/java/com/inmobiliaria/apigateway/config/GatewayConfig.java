73d03de3b7aeeebfa8d4e0b06cdff55ca5ceb3b5
repunksonn <ren.navarrete@duocuc.cl>
2026-05-19 18:29:59 -0400

    inicio de proyecto

--- api-gateway/src/main/java/com/inmobiliaria/apigateway/config/GatewayConfig.java
+++
@@ -1,8 +0,0 @@
-package com.inmobiliaria.apigateway.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class GatewayConfig {
-    // Vacío temporalmente para desactivar las rutas automáticas de Spring Cloud
-}
