6db6693702e84c7b8738202c08317419b1156828
repunksonn <ren.navarrete@duocuc.cl>
2026-05-20 03:07:27 -0400

    inicio de proyecto

--- apigateway/src/main/java/com/inmobiliaria/apigateway/filter/LoggingFilter.java
+++ apigateway/src/main/java/com/inmobiliaria/apigateway/filter/LoggingFilter.java
@@ -1,19 +1,16 @@
-package com.inmobiliaria.apigateway.filter;
 
-
+package com.arriendos.apigateway.filter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
 
 // @Component registra este filtro automaticamente en Spring
 // OncePerRequestFilter garantiza que el filtro
